2e5de6ae0d3bf63c326142613ab0ea926396e4e9
Hoàng Tấn <ithoangtan@gmail.com>
2019-10-19 17:18:22 +0700

    package lowdb: nodemon.json-ignore file

--- EXPRESS-CoderX/index.js
+++ EXPRESS-CoderX/index.js
@@ -1,53 +1,56 @@
-// use req.query
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
+var low = require("lowdb");
+var FileSync = require("lowdb/adapters/FileSync");
+var adapter = new FileSync("db.json");
+
+db = low(adapter);
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [] }).write();
 
 var port = 8000;
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.set("view engine", "pug");
+app.set("views", "./views");
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-var users = [
-  { id: 1, name: 'Thinh' },
-  { id: 2, name: 'Hung' }
-];
-
-app.get('/', function(req, res) {
-  res.render('index', {
-    name: 'AAA'
+app.get("/", function(req, res) {
+  res.render("index", {
+    name: "AAA"
   });
 });
 
-app.get('/users', function(req, res) {
-  res.render('users/index', {
-    users: users
+app.get("/users", function(req, res) {
+  res.render("users/index", {
+    users: db.get("users").value()
   });
 });
 
-app.get('/users/search', function(req, res) {
+app.get("/users/search", function(req, res) {
   var q = req.query.q;
-  var matchedUsers = users.filter(function(user) {
+  var matchedUsers = db.get("users").value().filter(function(user) {
     return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
   });
-
-  res.render('users/index', {
+  res.render("users/index", {
     users: matchedUsers
   });
 });
 
-app.get('/users/create', function(req, res) {
-  res.render('users/create');
+app.get("/users/create", function(req, res) {
+  res.render("users/create");
 });
 
-app.post('/users/create', function(req, res) {
-  users.push(req.body);
-  res.redirect('/users');
+app.post("/users/create", function(req, res) {
+  db.get("users")
+    .push(req.body)
+    .write();
+  res.redirect("/users");
 });
 
 app.listen(port, function() {
-  console.log('Server listening on port ' + port);
+  console.log("Server listening on port " + port);
 });
