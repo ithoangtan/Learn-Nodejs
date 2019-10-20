11f754e91c7fc9885a5e5fb17adc5a7bd35787b8
Hoàng Tấn <ithoangtan@gmail.com>
2019-10-20 10:38:36 +0700

    route source code

--- EXPRESS-CoderX/index.js
+++ EXPRESS-CoderX/index.js
@@ -1,14 +1,6 @@
 var express = require("express");
-var app = express();
 var bodyParser = require("body-parser");
-var low = require("lowdb");
-var FileSync = require("lowdb/adapters/FileSync");
-var adapter = new FileSync("db.json");
-
-db = low(adapter);
-
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ users: [] }).write();
+var app = express();
 
 var port = 8000;
 
@@ -18,38 +10,15 @@ app.set("views", "./views");
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+var usersRoute = require('./routes/users.route');
+
 app.get("/", function(req, res) {
   res.render("index", {
     name: "AAA"
   });
 });
 
-app.get("/users", function(req, res) {
-  res.render("users/index", {
-    users: db.get("users").value()
-  });
-});
-
-app.get("/users/search", function(req, res) {
-  var q = req.query.q;
-  var matchedUsers = db.get("users").value().filter(function(user) {
-    return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-  });
-  res.render("users/index", {
-    users: matchedUsers
-  });
-});
-
-app.get("/users/create", function(req, res) {
-  res.render("users/create");
-});
-
-app.post("/users/create", function(req, res) {
-  db.get("users")
-    .push(req.body)
-    .write();
-  res.redirect("/users");
-});
+app.use("/users", usersRoute);
 
 app.listen(port, function() {
   console.log("Server listening on port " + port);
