e9e1010f03b89bb7cf501199f8929d10438c54a5
Hoàng Tấn <ithoangtan@gmail.com>
2019-10-20 10:47:30 +0700

    controller - MCV set up

--- EXPRESS-CoderX/routes/users.route.js
+++ EXPRESS-CoderX/routes/users.route.js
@@ -1,53 +1,15 @@
 var express = require("express");
-var shortid = require("shortid");
-
-var db = require("../db");
+var controller = require("../controllers/users.controller");
 
 var router = express.Router();
+router.get("/", controller.index);
 
-router.get("/", function(req, res) {
-  res.render("users/index", {
-    users: db.get("users").value()
-  });
-});
+router.get("/search", controller.search);
 
-router.get("/search", function(req, res) {
-  var q = req.query.q;
-  var matchedUsers = db
-    .get("users")
-    .value()
-    .filter(function(user) {
-      return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    });
+router.get("/create", controller.create);
 
-  res.render("users/index", {
-    users: matchedUsers
-  });
-});
+router.get("/:id", controller.get);
 
-router.get("/create", function(req, res) {
-  res.render("users/create");
-});
-
-router.get("/:id", function(req, res) {
-  var id = req.params.id;
-
-  var user = db
-    .get("users")
-    .find({ id: id })
-    .value();
-
-  res.render("users/view", {
-    user: user
-  });
-});
-
-router.post("/create", function(req, res) {
-  req.body.id = shortid.generate();
-  db.get("users")
-    .push(req.body)
-    .write();
-  res.redirect("/users");
-});
+router.post("/create", controller.postCreate);
 
 module.exports = router;
