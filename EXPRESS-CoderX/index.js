e7666449cba5aceafb0a73bfd01757246fb7f34d
Hoàng Tấn <ithoangtan@gmail.com>
2019-10-23 22:52:42 +0700

    login: cookie, authencation with middleware

--- EXPRESS-CoderX/index.js
+++ EXPRESS-CoderX/index.js
@@ -3,6 +3,9 @@ var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 
 var userRoute = require("./routes/users.route");
+var authRoute = require("./routes/auth.route");
+
+var authMiddleware = require("./middlewares/auth.middleware");
 
 var port = 8000;
 
@@ -23,7 +26,8 @@ app.get("/", function(req, res) {
   });
 });
 
-app.use("/users", userRoute);
+app.use("/users", authMiddleware.requireAuth, userRoute);
+app.use("/auth", authRoute);
 
 app.listen(port, function() {
   console.log("Server listening on port " + port);
--- EXPRESS-CoderX/routes/users.route.js
+++ EXPRESS-CoderX/routes/users.route.js
@@ -2,6 +2,7 @@ var express = require("express");
 
 var controller = require("../controllers/users.controller");
 var validate = require("../validate/users.validate");
+var authMiddleware = require("../middlewares/auth.middleware");
 
 var router = express.Router();
 
